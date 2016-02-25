3e6b195007df2beb1af9e67b3a581a63af9860a3
Mbob023 <armcgrathmedia@gmail.com>
2016-02-25 11:27:27 -0600

    upgrade array to obj challenge

--- solo_script.js
+++ solo_script.js
@@ -1,50 +1,70 @@
 // ! ! !
 // Three Bugs
 
-var arrayAtticus = ["Atticus", "2405", "47000", 3];
-var arrayJem = ["Jem", "62347", "63500", 4];
-var arrayBoo = ["Boo", "11435", "54000", 3];
-var arrayScout = ["Scout", "6243", "74750", 5];
+//var arrayAtticus = ["Atticus", "2405", "47000", 3];
+//var arrayJem = ["Jem", "62347", "63500", 4];
+// var arrayBoo = ["Boo", "11435", "54000", 3];
+// var arrayScout = ["Scout", "6243", "74750", 5];
 
-var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
+
+
+
+function Person(objName, objNumber, objSalary, objReview){
+  this.objName = objName;
+  this.objNumber = objNumber;
+  this.objSalary = objSalary;
+  this.objReview = objReview;
+}
+
+var objAtticus = new Person("Atticus", "2405", "47000", 3);
+var objJem = new Person("Jem", "62347", "63500", 4);
+var objBoo = new Person("Boo", "11435", "54000", 3);
+var objScout = new Person("Scout", "6243", "74750", 5);
+
+var array = [objAtticus, objJem, objBoo, objScout];
+
+console.log(array);
 
 //Create variables used to write to the DOM
 var newEl, newText, position;
+
+
 //Capture the position of insertion into the DOM
 position = document.getElementById('content');
 
 //Loop the array, extracting each array and writing information to the DOM
 //Note that the information is not 'clean'
 for(var i = 0; i < array.length; i++){
-	array[i] = calculateSTI(array[i]);										//(array) should be array[i]
+  array[i] = calculateSTI(array[i]);
  	newEl = document.createElement('li');
 	newText = document.createTextNode(array[i]);
 	newEl.appendChild(newText);
 	position.appendChild(newEl);
 }
 
-function calculateSTI(array){
+
+
+function calculateSTI(person){
   var newArray = [];
-  	newArray[0] = array[0];
-		// newArray[1] = array[1];
-		// newArray[2] = array[2];
-		// newArray[3] = array[3];
+  newArray[0] = person.objName;
 
-	var employeeNumber = array[1];
-	var baseSalary = array[2];
-	var reviewScore = array[3];
+  var employeeNumber = person.objNumber;
+  var baseSalary = person.objSalary;
+  var reviewScore = person.objReview;
 
-  var bonus = Math.round(getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary));
+
+
+  var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
   if(bonus > 0.13){
-  bonus = 0.13;
+    bonus = 0.13;
   }
 
   newArray[1] = bonus;
-  newArray[2] = baseSalary * (1.0 + bonus);
-  newArray[3] = baseSalary * bonus;
+  //3rd bug I found - needed to round salary and bonus
+  newArray[2] = Math.round(baseSalary * (1.0 + bonus));
+  newArray[3] = Math.round(baseSalary * bonus);
   console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
   return newArray;
-	console.log(newArray);
 }
 
 function getBaseSTI(reviewScore){
@@ -66,9 +86,13 @@ function getBaseSTI(reviewScore){
       basePercent = 0.10;
       break;
   }
-  return basePercent; 														//Should this be - 1? NO(at least that's my thought)!
-																									//Removed - 1 from line 69 that returns basePercent.
+  return basePercent; 
+  //second bug!! I actually noticed this first but testing was no good until finding 1st bug
+
+
 }
+
+
 
 function getYearAdjustment(employeeNumber){
   var yearAdjustment = 0;
@@ -76,15 +100,14 @@ function getYearAdjustment(employeeNumber){
     yearAdjustment = 0.05;
   }
   return yearAdjustment;
-	// parseInt(employeeNumber);
 }
 
-function getIncomeAdjustment(baseSalary){					//I thought updating the salary to baseSalary would fix the NaN
-  var incomeAdjustment = 0;												//problem and convert the 'salary' or baseSalary from a string to
-  baseSalary = parseInt(baseSalary);							//a number. It didn't seem to make a difference.
-  if(baseSalary > 65000){
+function getIncomeAdjustment(salary){
+  var incomeAdjustment = 0;
+  salary = parseInt(salary);
+  if(salary > 65000){
     incomeAdjustment = 0.01;
   }
   return incomeAdjustment;
-																									//Tried this too: return baseSalary;
-}																									//Did not work. Fixing the (array) up top cleared the NaN.
+}
+
