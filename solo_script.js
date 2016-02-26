3d32cacf2b401a43d292f17922a7fb34a73f80b8
Mbob023 <armcgrathmedia@gmail.com>
2016-02-26 10:13:45 -0600

    update three bugs final challenge

--- solo_script.js
+++ solo_script.js
@@ -6,17 +6,7 @@
 // var arrayBoo = ["Boo", "11435", "54000", 3];
 // var arrayScout = ["Scout", "6243", "74750", 5];
 
-$(document).ready(fucntion(){
-  
-});
-
-
-function Person(objName, objNumber, objSalary, objReview){
-  this.objName = objName;
-  this.objNumber = objNumber;
-  this.objSalary = objSalary;
-  this.objReview = objReview;
-}
+var array = [];
 
 var objAtticus = new Person("Atticus", "2405", "47000", 3);
 var objJem = new Person("Jem", "62347", "63500", 4);
@@ -26,47 +16,78 @@ var objScout = new Person("Scout", "6243", "74750", 5);
 var array = [objAtticus, objJem, objBoo, objScout];
 
 console.log(array);
+$(document).ready(function(){
+  for(var i = 0; i < array.length; i++){
+    appendDom(calculateSTI(array[i]));
+  }
+});
+
+
+// var objAtticus = new Person("Atticus", "2405", "47000", 3);
+// var objJem = new Person("Jem", "62347", "63500", 4);
+// var objBoo = new Person("Boo", "11435", "54000", 3);
+// var objScout = new Person("Scout", "6243", "74750", 5);
+
+function Person(objName, objNumber, objSalary, objReview, bonus, newSalary, sti){
+  this.objName = objName;
+  this.objNumber = objNumber;
+  this.objSalary = objSalary;
+  this.objReview = objReview;
+  this.bonus = bonus;
+  this.newSalary = newSalary;
+  this.sti= sti;
+}
+
+// function NewPerson(objName, objSalary, newSalary, sti) {
+//   this.objName = objName;
+//   this.objSalary = objSalary;
+//   this.newSalary = newSalary;
+//   this.sti = sti;
+// }
+//function
+
+
+/// Previous Code...
+// var array = [objAtticus, objJem, objBoo, objScout];
+
 
 //Create variables used to write to the DOM
-var newEl, newText, position;
+// var newEl, newText, position;
 
 
 //Capture the position of insertion into the DOM
-position = document.getElementById('content');
+// position = document.getElementById('content');
 
 //Loop the array, extracting each array and writing information to the DOM
 //Note that the information is not 'clean'
-for(var i = 0; i < array.length; i++){
-  array[i] = calculateSTI(array[i]);
- 	newEl = document.createElement('li');
-	newText = document.createTextNode(array[i]);
-	newEl.appendChild(newText);
-	position.appendChild(newEl);
-}
+// for(var i = 0; i < array.length; i++){
+//   array[i] = calculateSTI(array[i]);
+//  	newEl = document.createElement('li');
+// 	newText = document.createTextNode(array[i]);
+// 	newEl.appendChild(newText);
+// 	position.appendChild(newEl);
+// }
 
 
 
 function calculateSTI(person){
-  var newArray = [];
-  newArray[0] = person.objName;
+  // var employeeObj = new Person (person.objName, person.employeeNumber, person.baseSalary, person.reviewScore);
+  // var newArray[0]= person.objName; ???
+  // var name = this.objName;
+  // var employeeNumber = this.objNumber;
+  // var baseSalary = this.objSalary;
+  // var reviewScore = this.objReview;
 
-  var employeeNumber = person.objNumber;
-  var baseSalary = person.objSalary;
-  var reviewScore = person.objReview;
-
-
-
-  var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
+  var bonus = getBaseSTI(person.objReview) + getYearAdjustment(person.objNumber) - getIncomeAdjustment(person.objSalary);
   if(bonus > 0.13){
     bonus = 0.13;
   }
 
-  newArray[1] = bonus;
+  person.bonus = bonus;
   //3rd bug I found - needed to round salary and bonus
-  newArray[2] = Math.round(baseSalary * (1.0 + bonus));
-  newArray[3] = Math.round(baseSalary * bonus);
-  console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
-  return newArray;
+  person.newSalary = Math.round(person.objSalary * (1.0 + bonus));
+  person.sti = Math.round(person.objSalary * bonus);
+  return person;
 }
 
 function getBaseSTI(reviewScore){
@@ -94,10 +115,9 @@ function getBaseSTI(reviewScore){
 
 }
 
-
-
 function getYearAdjustment(employeeNumber){
   var yearAdjustment = 0;
+  var employeeNumber = 0;
   if(employeeNumber.length == 4){
     yearAdjustment = 0.05;
   }
@@ -112,3 +132,13 @@ function getIncomeAdjustment(salary){
   }
   return incomeAdjustment;
 }
+
+
+function appendDom (person) {
+  $('.container').append('<div class="employees"></div>');
+  var $el = $('.container').children().last();
+  $el.append('<p>' + person.objName + '</p>');
+}
+
+// console.log(calculateSTI(employeeObj));
+// console.log(calculateSTI(array));
